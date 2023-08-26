f70acdf4ec7b3678a6cb08939c1b4f48517beef8
pradhumngautam <pradhumngautam0506@gmail.com>
2023-08-26 23:08:49 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -9,30 +9,30 @@ if (playGame){
         playerOne === "scissors"
     ){
       
-     let computerChoice =Math.floor(Math.random()*3+1);
-     let Computer =
-       computerChoice === 1 
-         ?"rock" 
-         :computerChoice === 2 
-         ?"paper" 
-         :"scissors";
+        let computerChoice =Math.floor(Math.random()*3+1);
+        let Computer =
+          computerChoice === 1 
+          ?"rock" 
+          :computerChoice === 2 
+          ?"paper" 
+          :"scissors";
 
-      let result =
-       playerOne = Computer
-         ?"Tie game!"
-         :playerOne === "rock" && Computer === "paper"
-         ?`playerOne : ${playerOne}\n Computer: ${Computer}\nComputer wins!`
-         :playerOne === "paper" && Computer === "scissors"
-         ?`playerOne : ${playerOne}\n Computer :${Computer}\nComputer wins!`
-         :playerOne === "scissors" && Computer === "rock"
-         ?`playerOne : ${playerOne}\nComputer :${Computer}\nComputer wins!`
-         :`playerOne : ${playerOne}\nComputer: ${Computer}\nplayerOne wins!`;
-      alert(result);
-      let playagain = confirm("playagain?");
-      playagain ? location.reload(): alert ("ok ,thanks for playing");
-     }else {
+        let result =
+          playerOne = Computer
+           ?"Tie game!"
+           :playerOne === "rock" && Computer === "paper"
+           ?`playerOne : ${playerOne}\n Computer: ${Computer}\nComputer wins!`
+           :playerOne === "paper" && Computer === "scissors"
+           ?`playerOne : ${playerOne}\n Computer :${Computer}\nComputer wins!`
+           :playerOne === "scissors" && Computer === "rock"
+           ?`playerOne : ${playerOne}\nComputer :${Computer}\nComputer wins!`
+           :`playerOne : ${playerOne}\nComputer: ${Computer}\nplayerOne wins!`;
+        alert(result);
+        let playagain = confirm("playagain?");
+        playagain ? location.reload(): alert ("ok ,thanks for playing");
+      }else {
       alert ("you didnt enter rock, paper or scissors.")
-     }
+      }
   }else {
     alert("I guess you changed your mind. Maybe next time")
   }
